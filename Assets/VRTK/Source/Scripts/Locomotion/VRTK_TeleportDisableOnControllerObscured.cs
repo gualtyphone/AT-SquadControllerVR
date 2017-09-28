a36ca651a9d9502d8e366c64c8200289a16b559e
gualtyphone <gualtyphone@me.com>
2017-09-28 19:28:34 +0100

    Initial commit

---
+++ Assets/VRTK/Source/Scripts/Locomotion/VRTK_TeleportDisableOnControllerObscured.cs
@@ -0,0 +1,71 @@
+﻿// Teleport Disable On Controller Obscured|Locomotion|20050
+namespace VRTK
+{
+    using UnityEngine;
+    using System.Collections;
+
+    /// <summary>
+    /// Prevents teleportation when the controllers are obscured from line of sight of the HMD.
+    /// </summary>
+    /// <remarks>
+    /// **Required Components:**
+    ///  * `VRTK_BasicTeleport` - A Teleport script to utilise for teleporting the play area.
+    ///  * `VRTK_HeadsetControllerAware` - A Headset Controller Aware script to determine when the HMD has line of sight to the controllers.
+    ///
+    /// **Script Usage:**
+    ///  * Place the `VRTK_TeleportDisableOnControllerObscured` script on the same GameObject as the active Teleport script.
+    /// </remarks>
+    [RequireComponent(typeof(VRTK_HeadsetControllerAware))]
+    [AddComponentMenu("VRTK/Scripts/Locomotion/VRTK_TeleportDisableOnControllerObscured")]
+    public class VRTK_TeleportDisableOnControllerObscured : MonoBehaviour
+    {
+        protected VRTK_BasicTeleport basicTeleport;
+        protected VRTK_HeadsetControllerAware headset;
+
+        protected virtual void OnEnable()
+        {
+            basicTeleport = GetComponent<VRTK_BasicTeleport>();
+            StartCoroutine(EnableAtEndOfFrame());
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (basicTeleport == null)
+            {
+                return;
+            }
+
+            if (headset != null)
+            {
+                headset.ControllerObscured -= new HeadsetControllerAwareEventHandler(DisableTeleport);
+                headset.ControllerUnobscured -= new HeadsetControllerAwareEventHandler(EnableTeleport);
+            }
+        }
+
+        protected virtual IEnumerator EnableAtEndOfFrame()
+        {
+            if (basicTeleport == null)
+            {
+                yield break;
+            }
+            yield return new WaitForEndOfFrame();
+
+            headset = VRTK_ObjectCache.registeredHeadsetControllerAwareness;
+            if (headset != null)
+            {
+                headset.ControllerObscured += new HeadsetControllerAwareEventHandler(DisableTeleport);
+                headset.ControllerUnobscured += new HeadsetControllerAwareEventHandler(EnableTeleport);
+            }
+        }
+
+        protected virtual void DisableTeleport(object sender, HeadsetControllerAwareEventArgs e)
+        {
+            basicTeleport.ToggleTeleportEnabled(false);
+        }
+
+        protected virtual void EnableTeleport(object sender, HeadsetControllerAwareEventArgs e)
+        {
+            basicTeleport.ToggleTeleportEnabled(true);
+        }
+    }
+}
---
+++ Assets/VRTK/Source/Scripts/Locomotion/VRTK_TeleportDisableOnHeadsetCollision.cs
@@ -0,0 +1,70 @@
+﻿// Teleport Disable On Headset Collision|Locomotion|20040
+namespace VRTK
+{
+    using UnityEngine;
+    using System.Collections;
+
+    /// <summary>
+    /// Prevents teleportation when the HMD is colliding with valid geometry.
+    /// </summary>
+    /// <remarks>
+    /// **Required Components:**
+    ///  * `VRTK_BasicTeleport` - A Teleport script to utilise for teleporting the play area.
+    ///  * `VRTK_HeadsetCollision` - A Headset Collision script for detecting when the headset has collided with valid geometry.
+    ///
+    /// **Script Usage:**
+    ///  * Place the `VRTK_TeleportDisableOnHeadsetCollision` script on the same GameObject as the active Teleport script.
+    /// </remarks>
+    [AddComponentMenu("VRTK/Scripts/Locomotion/VRTK_TeleportDisableOnHeadsetCollision")]
+    public class VRTK_TeleportDisableOnHeadsetCollision : MonoBehaviour
+    {
+        protected VRTK_BasicTeleport basicTeleport;
+        protected VRTK_HeadsetCollision headsetCollision;
+
+        protected virtual void OnEnable()
+        {
+            basicTeleport = GetComponent<VRTK_BasicTeleport>();
+            StartCoroutine(EnableAtEndOfFrame());
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (basicTeleport == null)
+            {
+                return;
+            }
+
+            if (headsetCollision != null)
+            {
+                headsetCollision.HeadsetCollisionDetect -= new HeadsetCollisionEventHandler(DisableTeleport);
+                headsetCollision.HeadsetCollisionEnded -= new HeadsetCollisionEventHandler(EnableTeleport);
+            }
+        }
+
+        protected virtual IEnumerator EnableAtEndOfFrame()
+        {
+            if (basicTeleport == null)
+            {
+                yield break;
+            }
+            yield return new WaitForEndOfFrame();
+
+            headsetCollision = VRTK_ObjectCache.registeredHeadsetCollider;
+            if (headsetCollision != null)
+            {
+                headsetCollision.HeadsetCollisionDetect += new HeadsetCollisionEventHandler(DisableTeleport);
+                headsetCollision.HeadsetCollisionEnded += new HeadsetCollisionEventHandler(EnableTeleport);
+            }
+        }
+
+        protected virtual void DisableTeleport(object sender, HeadsetCollisionEventArgs e)
+        {
+            basicTeleport.ToggleTeleportEnabled(false);
+        }
+
+        protected virtual void EnableTeleport(object sender, HeadsetCollisionEventArgs e)
+        {
+            basicTeleport.ToggleTeleportEnabled(true);
+        }
+    }
+}
