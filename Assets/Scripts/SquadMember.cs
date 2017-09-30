62af9c0babbc77d149879d0514c3dccbc5b5bab9
gualtyphone <gualtyphone@me.com>
2017-09-30 14:20:01 +0100

    Added basic navmesh and waypoint following

---
+++ Assets/Scripts/SquadMember.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SquadMember : MonoBehaviour {
+
+    public Transform destination;
+    public NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        destination = null;
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (destination != null)
+        {
+            agent.destination = destination.position;
+        }
+	}
+}
