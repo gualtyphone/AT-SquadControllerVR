1cdee2ee6fc62ba779bee2db106fba905661bc6a
gualtyphone <gualtyphone@me.com>
2018-04-10 01:09:20 +0100

    Buit for Hand-In

--- Assets/Scripts/CoverSpotsEditor.cs
+++ Assets/Scripts/CoverSpotsEditor.cs
@@ -1,34 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEditor;
+//using UnityEditor;
 
-[CustomEditor(typeof(FindCoverSpots))]
-public class CoverSpotsEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+//[CustomEditor(typeof(FindCoverSpots))]
+//public class CoverSpotsEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        DrawDefaultInspector();
 
-        FindCoverSpots myScript = (FindCoverSpots)target;
-        if (GUILayout.Button("Clear Data"))
-        {
-            myScript.ClearData();
-            SceneView.RepaintAll();
-        }
-        if (GUILayout.Button("Find Navmesh Edges"))
-        {
-            myScript.FindBorderEdges();
-            SceneView.RepaintAll();
-        }
-        if (GUILayout.Button("Find CoverSpots"))
-        {
-            myScript.CreateCoverSpotsEditor();
-            SceneView.RepaintAll();
-        }
-        if (GUILayout.Button("CleanUp CoverSpots"))
-        { 
-            myScript.CleanCoverSpotsEditor();
-            SceneView.RepaintAll();
-        }
-    }
-}
+//        FindCoverSpots myScript = (FindCoverSpots)target;
+//        if (GUILayout.Button("Clear Data"))
+//        {
+//            myScript.ClearData();
+//            SceneView.RepaintAll();
+//        }
+//        if (GUILayout.Button("Find Navmesh Edges"))
+//        {
+//            myScript.FindBorderEdges();
+//            SceneView.RepaintAll();
+//        }
+//        if (GUILayout.Button("Find CoverSpots"))
+//        {
+//            myScript.CreateCoverSpotsEditor();
+//            SceneView.RepaintAll();
+//        }
+//        if (GUILayout.Button("CleanUp CoverSpots"))
+//        { 
+//            myScript.CleanCoverSpotsEditor();
+//            SceneView.RepaintAll();
+//        }
+//    }
+//}
--- Assets/Scripts/SquadMasterEditor.cs
+++ Assets/Scripts/SquadMasterEditor.cs
@@ -1,33 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEditor;
+//using UnityEditor;
 
-[CustomEditor(typeof(SquadMaster))]
-public class SquadMasterEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
+//[CustomEditor(typeof(SquadMaster))]
+//public class SquadMasterEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        DrawDefaultInspector();
 
-        SquadMaster myScript = (SquadMaster)target;
-        if (GUILayout.Button("Merge All Squads"))
-        {
-            myScript.mergeSquads(myScript.SquadIDs);
-        }
-        //if (GUILayout.Button("Find Navmesh Edges"))
-        //{
-        //    myScript.FindBorderEdges();
-        //    SceneView.RepaintAll();
-        //}
-        //if (GUILayout.Button("Find CoverSpots"))
-        //{
-        //    myScript.CreateCoverSpotsEditor();
-        //    SceneView.RepaintAll();
-        //}
-        //if (GUILayout.Button("CleanUp CoverSpots"))
-        //{
-        //    myScript.CleanCoverSpotsEditor();
-        //    SceneView.RepaintAll();
-        //}
-    }
-}
+//        SquadMaster myScript = (SquadMaster)target;
+//        if (GUILayout.Button("Merge All Squads"))
+//        {
+//            myScript.mergeSquads(myScript.SquadIDs);
+//        }
+//        //if (GUILayout.Button("Find Navmesh Edges"))
+//        //{
+//        //    myScript.FindBorderEdges();
+//        //    SceneView.RepaintAll();
+//        //}
+//        //if (GUILayout.Button("Find CoverSpots"))
+//        //{
+//        //    myScript.CreateCoverSpotsEditor();
+//        //    SceneView.RepaintAll();
+//        //}
+//        //if (GUILayout.Button("CleanUp CoverSpots"))
+//        //{
+//        //    myScript.CleanCoverSpotsEditor();
+//        //    SceneView.RepaintAll();
+//        //}
+//    }
+//}
